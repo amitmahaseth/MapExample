43970b8b7de9e24158aa2a12cd6f5d084ecedb93
amitmahaseth <amitmahaseth6@gmail.com>
2021-12-28 17:40:33 +0530

    second

--- app/src/main/java/com/example/mapexample/MainActivity.kt
+++ app/src/main/java/com/example/mapexample/MainActivity.kt
@@ -19,13 +19,101 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import java.util.jar.Manifest
+import android.content.IntentSender
+import android.content.IntentSender.SendIntentException
+
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationRequest
+
+import com.google.android.gms.location.LocationSettingsResponse
+
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import java.lang.Exception
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var arrayData: Array<String>
     private lateinit var mMap: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        enableLocationSettings()
+
+
+    }
+
+    protected fun enableLocationSettings() {
+        val locationRequest = LocationRequest.create()
+            .setInterval((10 * 1000).toLong())
+            .setFastestInterval((2 * 1000).toLong())
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        LocationServices
+            .getSettingsClient(this)
+            .checkLocationSettings(builder.build())
+            .addOnSuccessListener(
+                this
+            ) { response: LocationSettingsResponse? ->
+               arrayData= arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (this.checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(arrayData, 456)
+
+                    }
+                } else {
+                    TODO("VERSION.SDK_INT < M")
+                }
+
+
+
+            }
+            .addOnFailureListener(
+                this
+            ) { ex: Exception? ->
+                if (ex is ResolvableApiException) {
+                    // Location settings are NOT satisfied,  but this can be fixed  by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),  and check the result in onActivityResult().
+                        ex.startResolutionForResult(
+                            this@MainActivity,
+                            123
+                        )
+                    } catch (sendEx: SendIntentException) {
+                        // Ignore the error.
+                    }
+                }
+            }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 123) {
+            enableLocationSettings()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults[0]==PackageManager.PERMISSION_GRANTED)
+        {
+            showMapData()
+        }else
+        {
+            enableLocationSettings()
+        }
+    }
+
+    fun showMapData()
+    {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment)
                 as SupportMapFragment
         mapFragment.getMapAsync(OnMapReadyCallback {
@@ -42,51 +130,10 @@ class MainActivity : AppCompatActivity() {
                     android.Manifest.permission.ACCESS_COARSE_LOCATION
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
 
             }
             mMap.isMyLocationEnabled = true
         })
 
-        val locationPermissionRequest = registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ) { permissions ->
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                when {
-                    permissions.getOrDefault(
-                        android.Manifest.permission.ACCESS_FINE_LOCATION,
-                        false
-                    ) -> {
-                        // Precise location access granted.
-                    }
-                    permissions.getOrDefault(
-                        android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                        false
-                    ) -> {
-                        // Only approximate location access granted.
-                    }
-                    else -> {
-                        // No location access granted.
-                    }
-                }
-            }
-        }
-
-        locationPermissionRequest.launch(
-            arrayOf(
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            )
-        )
-
-
     }
-
-
 }
